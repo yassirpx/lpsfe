72e013fe55e8b2379b87cdf4b9497b63e976c1ed
unknown <haiddouchy3@gmail.com>
2023-06-06 22:00:38 +0100

    last commit

--- resources/views/form/annee.blade.php
+++ resources/views/form/annee.blade.php
@@ -24,6 +24,7 @@
 <div class="card-box pb-10">
     @if ($errors->any())
     <div class="alert alert-danger">
+        
         @foreach ($errors->all() as $error)
             <p>{{ $error }}</p>
         @endforeach
